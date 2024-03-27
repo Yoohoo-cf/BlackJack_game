44dc51ac768d3d8352fcc355f1607a1135fd259f
Remy <wangrui777111@gmail.com>
2024-03-27 19:15:07 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import random
+
+# both sides are inclusive
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+
+# not include 1
+random_float = random.random()
+print(random_float)
